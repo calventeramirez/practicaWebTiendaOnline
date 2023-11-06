3df0536eab158a4ea5a746a305a326c153307aae
Pablo Calvente <calventeramirez@outlook.com>
2023-11-06 13:09:45 +0100

    Añadido index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <?php require "conecta_bbdd.php" ?>
+</head>
+
+<body>
+    <?php
+    $sql = "SELECT * FROM productos";
+    $resultado = $conexion->query($sql);
+    ?>
+    
+    <div class="container">
+        <h1>Inicio</h1>
+        <table class="table table-secondary table-hover">
+            <thead class="table table-striped">
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                    <th>Descripcion</th>
+                    <th>Cantidad</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                while ($fila = $resultado->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $fila["idProducto"] . "</td>";
+                    echo "<td>" . $fila["nombreProducto"] . "</td>";
+                    echo "<td>" . $fila["precio"] . "</td>";
+                    echo "<td>" . $fila["descripcion"] . "</td>";
+                    echo "<td>" . $fila["cantidad"] . "</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
