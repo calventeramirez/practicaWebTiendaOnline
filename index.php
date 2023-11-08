43478c345b9cb823f329aade0bb54aec6968c1b1
Pablo Calvente <calventeramirez@outlook.com>
2023-11-08 09:17:15 +0100

    Modificado index para añadir la imagen a la tabla de productos y se usa Objetos para sacar el producto

--- index.php
+++ index.php
@@ -7,12 +7,18 @@
     <title>Inicio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <?php require "conecta_bbdd.php" ?>
+    <?php require "Objetos/producto.php" ?>
 </head>
 
 <body>
     <?php
     $sql = "SELECT * FROM productos";
     $resultado = $conexion->query($sql);
+    $productos = [];
+    while($fila = $resultado->fetch_assoc()){
+        $nuevo_producto = new Producto($fila["idProducto"], $fila["nombreProducto"], $fila["precio"], $fila["descripcion"], $fila["cantidad"], $fila["imagen"]);
+        array_push($productos, $nuevo_producto);
+    }
     ?>
     
     <div class="container">
@@ -25,17 +31,19 @@
                     <th>Precio</th>
                     <th>Descripcion</th>
                     <th>Cantidad</th>
+                    <th>Imagen</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
-                while ($fila = $resultado->fetch_assoc()) {
+                foreach($productos as $producto){
                     echo "<tr>";
-                    echo "<td>" . $fila["idProducto"] . "</td>";
-                    echo "<td>" . $fila["nombreProducto"] . "</td>";
-                    echo "<td>" . $fila["precio"] . "</td>";
-                    echo "<td>" . $fila["descripcion"] . "</td>";
-                    echo "<td>" . $fila["cantidad"] . "</td>";
+                    echo "<td>" . $producto->idProducto . "</td>";
+                    echo "<td>" . $producto->nombreProducto . "</td>";
+                    echo "<td>" . $producto->precio . "</td>";
+                    echo "<td>" . $producto->descripcion . "</td>";
+                    echo "<td>" . $producto->cantidad . "</td>";
+                    echo "<td><img src = '".$producto->imagen."' alt ='' width = '50%' heigth = '50%'></td>";
                     echo "</tr>";
                 }
                 ?>
