71fbd1b9cc79d00ed9842c5026450afc1a167b29
Pablo Calvente <calventeramirez@outlook.com>
2023-11-17 14:36:35 +0100

    Añadido la cesta y funcionamiento para eliminar objeto de la cesta

--- views/cesta.php
+++ views/cesta.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="styles/estilos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <?php require "../util/conecta_bbdd.php" ?>
+    <?php require "../util/objetoCesta.php" ?>
 </head>
 
 <body>
@@ -23,18 +24,14 @@
             $precioTotal = $fila["precioTotal"];
         }
 
-        $sql2 = "SELECT * FROM productosCestas WHERE idCesta = $idCesta";
+        $sql2 = "SELECT p.nombreProducto as nombre, p.precio as precio, p.imagen as imagen, pc.cantidad as cantidad FROM productos p JOIN productosCestas pc ON p.idProducto = pc.idProducto WHERE pc.idCesta = $idCesta";
         $resultado2 = $conexion -> query($sql2);
 
-        while($fila = $resultado2->fetch_assoc()){
-            $idProducto = $fila["idProducto"];
-            $cantidad = $fila["cantidad"];
+        $productos = [];
+        while($fila = $resultado2 -> fetch_assoc()){
+            $nuevo_producto = new productoCesta($fila["nombre"], $fila["precio"], $fila["imagen"], $fila["cantidad"]);
+            array_push($productos, $nuevo_producto);
         }
-
-        $sql3 = "SELECT * FROM productos WHERE idProducto = $idProducto";
-        
-
-
     }
     ?>
     <div class="container">
@@ -70,17 +67,55 @@
             </nav>
         </header>
         <main>
-            <table>
-                <thead>
+            <table class = "table table-secondary table-hover">
+                <thead class="table table-striped">
                     <tr>
-                        <td>Nombre</td>
-                        <td>Imagen</td>
-                        <td>Cantidad</td>
+                        <th>Nombre</th>
+                        <th>Imagen</th>
+                        <th>Cantidad</th>
+                        <th>Precio Unitario</th>
+                        <th>Eliminar</th>
                     </tr>
                 </thead>
                 <tbody>
-                    
+                    <?php
+                        foreach($productos as $producto){
+                            echo "<tr>";
+                            echo "<td>" . $producto->nombre . "</td>";
+                            echo "<td><img src='$producto->imagen' width='50%' height='50%'></td>";
+                            echo "<td>" . $producto->cantidad . "</td>";
+                            echo "<td>" . $producto->precio . "</td>";
+                            ?>
+                            <td>
+                                <form action="eliminarProductoCesta.php" method="POST">
+                                    <?php
+                                        $sqlID = "SELECT idProducto FROM productos WHERE nombreProducto = '$producto->nombre'";
+                                        $resultadoID = $conexion -> query($sqlID);
+                                        while($fila = $resultadoID -> fetch_assoc()){
+                                            $idProducto = $fila["idProducto"];
+                                        }
+                                    ?>
+                                    <input type="hidden" name="id" value="<?php echo $idProducto ?>">
+                                    <?php
+                                        $precioEliminar  = $producto->precio * $producto->cantidad;
+                                        $precioTotal2 = $precioTotal - $precioEliminar;
+                                    ?>
+                                    <input type = "hidden" name = "precioTotal" value = "<?php echo $precioTotal2 ?>">
+                                    <input type="hidden" name = "idCesta" value="<?php echo $idCesta ?>">
+                                    <input type="submit" value="Eliminar" class="btn btn-danger">
+                                </form>
+                            </td>
+                            <?php
+                            echo "</tr>";
+                        }
+                    ?>
                 </tbody>
+                <tfoot class="table table-info">
+                    <tr>
+                        <td colspan="4">Total del carrito</td>
+                        <td><?php echo $precioTotal; ?></td>
+                    </tr>
+                </tfoot>
             </table>
         </main>
     </div>
---
+++ views/eliminarProductoCesta.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+    require "../util/conecta_bbdd.php";
+    $id = $_POST["id"];
+    echo "<p>$id</p>";
+    $precio = $_POST["precioTotal"];
+    $idCesta = $_POST["idCesta"];
+    $usuario = $_SESSION["usuario"];
+    $sql = "DELETE FROM productosCestas WHERE idProducto = $id";
+    $conexion -> query($sql);
+    $sql2 = "UPDATE Cestas SET precioTotal = '$precio'  WHERE idCesta = '$idCesta'";
+    $conexion -> query($sql2);
+    header("Location: cesta.php");
+?>
